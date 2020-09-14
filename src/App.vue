f50be274297d8da2923a5818c25f9a4c426cbbec
송지원 <jiwsong@ebay.com>
2020-09-15 08:31:24 +0900

    test for Tabulator 4.8.0 bug in vue.js (Tabulator object)

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<div ref="table"></div>
+
+<script>
+var Tabulator = require("tabulator-tables");
+// import Tabulator from 'tabulator-tables';
+
+  export default {
+    name: 'app',
+    data: function () {
+      return {
+        tabulator: null, //variable to hold your table
+        tableData: [], //data for table to display
+      }
+    },
+    watch: {
+      //update table if data changes
+      tableData: {
+        handler: function (newData) {
+          this.tabulator.replaceData(newData);
+        },
+        deep: true,
+      }
+    },
+    mounted() {
+      console.log(Tabulator);
+
+      //instantiate Tabulator when element is mounted
+      this.tabulator = new Tabulator(this.$refs.table, {
+        data: this.tableData, //link data to table
+        reactiveData:true, //enable data reactivity
+        columns: [], //define table columns
+      });
+    }
+  }
+</script>
